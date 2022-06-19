0e368c4faa5ee99fb0fd05b736722ab2875d8f70
LeOhtani17 <84379753+LeOhtani17@users.noreply.github.com>
2022-06-19 16:29:33 +0900

    Delete 授業課題7.py

--- 授業課題7.py
+++
@@ -1,9 +0,0 @@
-for i in range(1,101):
-    if int(i)%3==0:
-        print("Fizz")
-    elif int(i)%5==0:
-        print("Buzz")
-    elif int(i)%3==0 and int(i)%5==0:
-        print("FizzBuzz")
-    else:
-        print(str(i))
